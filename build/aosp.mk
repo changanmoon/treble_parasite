fd08d7df0a23f21f2ac08b464fc00af2ec82f050
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2024-04-24 23:22:31 +0800

    Update and rename aosp.mk to parasite.mk

--- build/aosp.mk
+++
@@ -1,4 +0,0 @@
-$(call inherit-product, vendor/ponces/config/common.mk)
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_aosp/android-14.0/config/ota.json
---
+++ build/parasite.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, vendor/ponces/config/common.mk)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/changanmoon/treble_parasite/14.0/config/ota.json
